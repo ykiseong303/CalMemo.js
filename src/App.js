1c5cee2f510e5ed6e2a2367c1e3bfec728e56724
ykiseong303 <41833412+ykiseong303@users.noreply.github.com>
2020-02-01 16:50:09 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
 import MemoInput from "./components/MemoInput";
-import Content from "./components/Content";
+// import Content from "./components/Content";
+import TOC from "./components/TOC";
 class App extends Component {
   constructor(props){
     super(props);
+    this.max_content_id = 0;
     this.state = {
-      desc : "",
-      contents:{month : '', day:'',desc:''},
+      contents:[
+        {id:'',month:'',day:'',desc:''}
+      ],
+      content:{month : '', day:'',desc:''},
     }
   }
-
+  DivideText(_text){
+    var _month = _text.substr(0,2);
+    var _day = _text.substr(2,2);
+    var _desc = _text.substr(4,);
+    var content ={month : _month,day:_day,desc:_desc,};
+    return content;
+}
   render() {
     console.log('====>App')
     return(
       <div className="App">
-        <h1>Melendar_test</h1>
-        <MemoInput onSubmit={function(_content){
-            this.setState(
-              {
-                contents : _content,
-              }
-            );
-          }.bind(this)}>
+        <h1>CalMemo_test</h1>
+        <MemoInput onSubmit={function(_text){
+          var content = this.DivideText(_text);
+          this.max_content_id = this.max_content_id + 1;
+          var _contents = this.state.contents.concat({
+            id : this.max_content_id,
+            month : content.month,
+            day : content.day,
+            desc : content.desc,
+          });
+          this.setState(
+            {
+              contents : _contents,
+            }
+          );
+        }.bind(this)}>
         </MemoInput>
-        <Content data={this.state.contents}></Content>
+        <TOC data = {this.state.contents}></TOC>
+        {/* <Content data={this.state.content}></Content> */}
       </div>
     );
   }
